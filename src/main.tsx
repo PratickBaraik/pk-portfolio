e1ed1cab103d616298ca1d2081f2103c61a966bf
Pratick Baraik <animeNerd.int@protonmail.com>
2026-03-07 15:44:16 +0530

    update import of work link in main file

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import HomePage from "./features/home/Home";
-import Works from "./features/works/works";
+import Works from "./features/works/Works";
 import Contact from "./features/contact/Contact";
 // import Gearlist from "./features/gearlist/Gearlist";
 
